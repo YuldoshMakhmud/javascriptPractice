15300364dd92966dd41f6cdd2d6a01c68e594773
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2023-12-30 08:42:23 -0800

    practice

---
+++ variables.js
@@ -0,0 +1,43 @@
+//let name = "mosh";
+// console.log(name);
+
+
+//   Role  
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with a number (1name)
+//cannot contain aspace or hypen (-)
+// are case-sensitive
+
+//let firsName = "yuldoshev";   //camel notion
+
+// constants  cannot change value
+
+//const number = 5
+
+// Primitives/ Value types      ------   Reference types
+// string                               
+// number 
+//boolean
+ // undefinrd
+ //null
+
+ // arifmetik amallar   + plus   - minus   * qushish / bulush
+
+ //  ++ = +1    -- = -1      +=  =  o'zgaruvchini o'z o'ziga tenglab qushih   -=  =  o'z o'ziga tenglab ayirish
+
+
+ let  number =1.5
+
+ console.log(Number.isInteger(number));  // number is integer butunmi .  boolean qiymat qaytaradi   
+
+
+ //   Stringni numberga utkazishni ikki yuli bor
+
+
+ console.log(Number.parseInt(number));
+
+
+
+
+

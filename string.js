0299355f9f8ab728208951ef17143794141637a9
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2023-12-31 04:49:56 -0800

    string

---
+++ string.js
@@ -0,0 +1,30 @@
+// let name = `maxmud asror muhammad`;
+// let text = "abbos"
+
+
+//console.log(name.length);   length uzunlik degani length hardoim 1dan boshlanadi length console dagi chiqarilsa  string ichidagi barcha ma'limotlarni sanab beradi
+// []  string ichidan malumot qidirush
+
+// console.log(name.charAt(5));  index no'ldan boshlab sanaydi
+//console.log(name.concat(" max"));   variablega ohiridan malumot qushish
+//console.log(`${ text}`);  uzgaruvchi ichiga uzgaruvchi qushish
+
+// console.log(text.indexOf(1,5)); uziga ikta qiymat oladi nimani qidirish va qayerdan
+// console.log(text.lastIndexOf(4)); ohiridan sanashni boshlaydi
+// console.log(text.search(1));
+// console.log(text.includes("abbos"));   boolean qiymat qaytaradi  bor yoki yuq
+
+
+//  \n   new line      \t   tab
+
+//  String ichidan ma'lumot qirqib olish
+
+ let text = "max , maxi , jack";
+
+//  console.log(text.slice(1 , 3));  o'z ichiga ikta qiymat oladi qayerdan  qayergacha
+
+// console.log(text.substring(0 , 3)); slice  bn birhil
+// console.log(text.substr(1,3));  ikta qiymat oladi qayerdan boshlash va nechta kesib olish
+
+console.log(text.replace("max", "rep"));  // almashtirish   ( old  ,   new )
+console.log(text.split());

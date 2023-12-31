8ef5008a0860b20f932de7c496cef593c4b4f607
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2023-12-31 06:15:05 -0800

    function

---
+++ funksiya.js
@@ -0,0 +1,21 @@
+//  function bu  block code
+// function 3hil yuli bor
+// 1- Decalaration function
+// 2- expression function
+// 3- Arrov function
+
+sayHello()
+ function sayHello(){
+    console.log("hello");
+ }
+
+
+ let sayHi = function(){
+    console.log("Hello");
+ }
+ sayHi()
+
+ hello = ()=>{
+    return "hello world"
+ }
+ console.log(hello());

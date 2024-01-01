27116b32131809e0f6b90f11972749f7a63434c2
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2023-12-31 20:49:58 -0800

    ternary

---
+++ farqlash.js
@@ -0,0 +1,37 @@
+ //  if else   farqlarni solishtirish  conditional operation
+// let haur = 19
+
+//  if (haur < 18) {
+// console.log("good morning");
+//  }else if (haur < 20){
+//     console.log("good day");
+//  }
+//  else {
+//         console.log("good evening");
+//     }
+
+// switch keys     bitta malumotni turli qilib tekshivaradi. switchda bita malumot aylanadi  .  ifda esa ko'p ma'lumotlar bn ishlanadi
+let day = 4
+
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
---
+++ ternary.js
@@ -0,0 +1,5 @@
+// ternary
+// ket word = ?
+
+// let name = "max"
+// name === "max" ? console.log("max"): console.log("max emas");   agar false bulsa : nuqtadan keyin uqishni boshledi
